7005b78ce171d6fdd3f01fc11d7606b951c4fb3f
jolew92 <joannalewicka@outlook.com>
2015-03-31 18:07:30 +0200

    modele

---
+++ ZTW/Filmland/settings.py
@@ -0,0 +1,102 @@
+"""
+Django settings for ZTW project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'd0mnhng&p6y)k&g!=ls6b-jr9hk#h*33t&_s%($n3=y+#5l-5h',
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'accounts',
+    'movies',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'Filmland.urls'
+
+WSGI_APPLICATION = 'Filmland.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ZTW',
+        'USER': 'root',
+        'PASSWORD': 'P@ssw0rd',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'pl'
+
+TIME_ZONE = 'Europe/Warsaw'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+SETTINGS_PATH = os.path.dirname(__file__)
+PROJECT_PATH = os.path.join(SETTINGS_PATH, os.pardir)
+PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+TEMPLATES_PATH = os.path.join(PROJECT_PATH, "templates")
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    TEMPLATES_PATH,
+)
---
+++ ZTW/movies/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from movies.models import Movie, Language, Genre, Country, Role, Person, Biography, Description, MovieRole
+
+
+class MoviePeopleInline(admin.TabularInline):
+    model = MovieRole
+    filter_horizontal = ("people",)
+    extra = 0
+
+
+class MovieCountryInline(admin.TabularInline):
+    model = Movie.country.through
+    extra = 0
+
+
+class PersonBioInline(admin.TabularInline):
+    model = Biography
+    extra = 0
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [PersonBioInline]
+    exclude = ('biography',)
+
+
+class DescriptionMovieInline(admin.TabularInline):
+    model = Description
+    extra = 0
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = 'title', 'year', 'language'
+    fields = 'title', 'year', 'language', 'genre'
+    filter_horizontal = ("genre",)
+    inlines = [MovieCountryInline, DescriptionMovieInline, MoviePeopleInline]
+    exclude = ('country', 'roles',)
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Country)
+admin.site.register(Role)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Biography)
+admin.site.register(Description)
+
+
